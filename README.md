b13d79aa9ec8368d9f5691c166bca3d732a56b5c
gareth-machine-user <garethgeorge97@gmail.com>
2019-11-04 03:48:26 +0000

    Add README and .ebextensions

---
+++ README.md
@@ -0,0 +1,2 @@
+eb create -db.engine postgres -db.i db.t2.micro -db.user u --envvars SECRET_KEY_BASE=Hooli --single Bachmanity 
+
---
+++ bachmanity-server/.ebextensions/scripts/migrate_db.sh
@@ -0,0 +1,16 @@
+if [ "${PROCESS}" = "WEB" ]; then
+
+  . /opt/elasticbeanstalk/hooks/common.sh
+
+  EB_SUPPORT_FILES=$(/opt/elasticbeanstalk/bin/get-config container -k support_files_dir)
+
+  EB_CONFIG_DOCKER_ENV_ARGS=()
+
+  while read -r ENV_VAR; do
+    EB_CONFIG_DOCKER_ENV_ARGS+=(--env "$ENV_VAR")
+  done < <($EB_SUPPORT_FILES/generate_env)
+
+  echo "Running migrations for aws_beanstalk/staging-app"
+  docker run --rm "${EB_CONFIG_DOCKER_ENV_ARGS[@]}" aws_beanstalk/staging-app bundle exec rake db:migrate || echo "The Migrations failed to run."
+fi
+true
