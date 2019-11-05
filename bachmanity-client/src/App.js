a5c233c177c0051a883045f71e2d827505a1dfdd
Gareth George <garethgeorge97@gmail.com>
2019-11-05 12:53:31 -0800

    ui changes

--- bachmanity-client/src/App.js
+++ bachmanity-client/src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import FeedView from "./views/feed_view";
+import LobbyView from "./views/lobby_view";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/lobby/:id" component={LobbyView} />
+          <Route path="/">
+            <FeedView />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
